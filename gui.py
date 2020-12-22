d99b114ba43886febbf7ee99564936bab9b4d33b
Stiveouissant <olek.busek@gmail.com>
2020-12-22 22:43:29 +0100

    gui part 2

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from PyQt5.QtWidgets import QTableView, QPushButton, QMenuBar, QMainWindow, QWidget
+from PyQt5.QtWidgets import QTableView, QPushButton, QMenuBar, QMainWindow, QWidget, QButtonGroup, QGroupBox
 from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout
 from PyQt5.QtWidgets import QMenu
 from PyQt5.QtCore import Qt
@@ -13,6 +13,27 @@ class Ui_Widget(object):
 
     def setup_ui(self, widget):
         widget.setObjectName("Widget")
+
+        # przyciski PushButton ###
+        button_layout_box = QHBoxLayout()
+        self.button_group = QButtonGroup()
+        self.button_group.setExclusive(False)
+        for v in ('Speech', 'Sound', 'Pitch'):
+            self.btn = QPushButton(v)
+            self.btn.setStyleSheet("background-color: lightblue")
+            self.button_group.addButton(self.btn)
+            button_layout_box.addWidget(self.btn)
+        # grupujemy przyciski
+        self.button_groupbox = QGroupBox('Recognition mode')
+        self.button_groupbox.setLayout(button_layout_box)
+        self.button_groupbox.setObjectName('Push')
+        # koniec PushButton ###
+
+        info_panel = QHBoxLayout()
+        label1 = QLabel("Profile: Profile 1", self)
+        label2 = QLabel("Placeholder for sound visuals", self)
+        info_panel.addWidget(label1)
+        info_panel.addWidget(label2)
 
         # table data view
         self.view = QTableView()
@@ -34,8 +55,7 @@ class Ui_Widget(object):
 
         # main view ###
         layoutV = QVBoxLayout(self)
+        layoutV.addWidget(self.button_groupbox)
+        layoutV.addLayout(info_panel)
         layoutV.addWidget(self.view)
         layoutV.addLayout(layout)
-
-        self.setWindowTitle("Modular Sound Control System")
-        self.resize(500, 300)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sounddevice
-from scipy.io.wavfile import write
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout, QMenuBar, QMenu, QAction
+# import sounddevice
+# from scipy.io.wavfile import write
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout, QMenuBar, QMenu, QAction, QStatusBar
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QLabel, QGridLayout
 from PyQt5.QtWidgets import QLineEdit, QPushButton, QHBoxLayout
@@ -25,6 +25,7 @@ class MainWindow(QMainWindow):
         self._createActions()
         self._connectActions()
         self._createMenuBar()
+        self._createStatusBar()
 
     def _createMenuBar(self):
         menu_bar = self.menuBar()
@@ -42,6 +43,10 @@ class MainWindow(QMainWindow):
         menu_bar.addMenu(file_menu)
         menu_bar.addMenu(settings_menu)
         menu_bar.addMenu(help_menu)
+
+    def _createStatusBar(self):
+        self.statusbar = QStatusBar()
+        self.setStatusBar(self.statusbar)
 
     def _createActions(self):
         self.change_profile_action = QAction("&Change Profile", self)
@@ -102,6 +107,9 @@ class MainWindow(QMainWindow):
 
 class MainWidget(QWidget, Ui_Widget):
 
+    # default recognition mode
+    recognition_mode = 'Speech'
+
     def __init__(self, parent=None):
         super(MainWidget, self).__init__(parent)
         self.setup_ui(self)
@@ -111,8 +119,24 @@ class MainWidget(QWidget, Ui_Widget):
         # self.dodajBtn.clicked.connect(self.dodaj)
         # self.zapiszBtn.clicked.connect(self.zapisz)
 
+        # przyciski PushButton ###
+        for btn in self.button_group.buttons():
+            btn.clicked.connect(self.mode_change)
+
+
     def koniec(self):
         self.close()
+
+    def mode_change(self):
+        which_button = self.sender()
+        for btn in self.button_group.buttons():
+            if which_button.text() == btn.text():
+                btn.setStyleSheet("background-color: lightgreen")
+            else:
+                btn.setStyleSheet("background-color: lightblue")
+        recognition_mode = which_button.text()
+        print(recognition_mode)
+
 
     # def zapisz(self):
     #     baza.zapiszDane(model.tabela)
@@ -167,73 +191,73 @@ class MainWidget(QWidget, Ui_Widget):
     #
 
 
-class Recorder(QWidget):
-
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-        self.interfejs()
-
-    def interfejs(self):
-
-        # etykiety
-        etykieta1 = QLabel("Naciśnij przycisk, aby rozpocząć nagrywanie:", self)
-
-
-        # przypisanie widgetów do układu tabelarycznego
-        ukladT = QGridLayout()
-        ukladT.addWidget(etykieta1, 0, 0)
-
-        # przyciski
-        koniecBtn = QPushButton("&Koniec", self)
-        recordBtn = QPushButton("&Nagraj", self)
-        koniecBtn.resize(koniecBtn.sizeHint())
-
-        ukladH = QHBoxLayout()
-        ukladH.addWidget(recordBtn)
-
-        ukladT.addLayout(ukladH, 2, 0, 1, 3)
-        ukladT.addWidget(koniecBtn, 3, 0, 1, 3)
-
-        # przypisanie utworzonego układu do okna
-        self.setLayout(ukladT)
-
-        koniecBtn.clicked.connect(self.koniec)
-        recordBtn.clicked.connect(self.voice_recording)
-
-
-        # self.liczba1Edt.setFocus()
-        self.setGeometry(20, 20, 300, 100)
-        # self.setWindowIcon(QIcon('icon.png'))
-        self.setWindowTitle("Voice recording")
-        self.show()
-
-    def voice_recording(self):
-        fs = 44100
-        second = 3
-        print("recording......")
-        record_voice = sounddevice.rec(int(second * fs), samplerate=fs, channels=1)
-        sounddevice.wait()
-        write("output.wav", fs, record_voice)
-
-    def koniec(self):
-        self.close()
-
-    def closeEvent(self, event):
-
-        odp = QMessageBox.question(
-            self, 'Info',
-            "Are you sure you want to exit?",
-            QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-
-        if odp == QMessageBox.Yes:
-            event.accept()
-        else:
-            event.ignore()
-
-    def keyPressEvent(self, e):
-        if e.key() == Qt.Key_Escape:
-            self.close()
+# class Recorder(QWidget):
+#
+#     def __init__(self, parent=None):
+#         super().__init__(parent)
+#
+#         self.interfejs()
+#
+#     def interfejs(self):
+#
+#         # etykiety
+#         etykieta1 = QLabel("Naciśnij przycisk, aby rozpocząć nagrywanie:", self)
+#
+#
+#         # przypisanie widgetów do układu tabelarycznego
+#         ukladT = QGridLayout()
+#         ukladT.addWidget(etykieta1, 0, 0)
+#
+#         # przyciski
+#         koniecBtn = QPushButton("&Koniec", self)
+#         recordBtn = QPushButton("&Nagraj", self)
+#         koniecBtn.resize(koniecBtn.sizeHint())
+#
+#         ukladH = QHBoxLayout()
+#         ukladH.addWidget(recordBtn)
+#
+#         ukladT.addLayout(ukladH, 2, 0, 1, 3)
+#         ukladT.addWidget(koniecBtn, 3, 0, 1, 3)
+#
+#         # przypisanie utworzonego układu do okna
+#         self.setLayout(ukladT)
+#
+#         koniecBtn.clicked.connect(self.koniec)
+#         recordBtn.clicked.connect(self.voice_recording)
+#
+#
+#         # self.liczba1Edt.setFocus()
+#         self.setGeometry(20, 20, 300, 100)
+#         # self.setWindowIcon(QIcon('icon.png'))
+#         self.setWindowTitle("Voice recording")
+#         self.show()
+#
+#     def voice_recording(self):
+#         fs = 44100
+#         second = 3
+#         print("recording......")
+#         record_voice = sounddevice.rec(int(second * fs), samplerate=fs, channels=1)
+#         sounddevice.wait()
+#         write("output.wav", fs, record_voice)
+#
+#     def koniec(self):
+#         self.close()
+#
+#     def closeEvent(self, event):
+#
+#         odp = QMessageBox.question(
+#             self, 'Info',
+#             "Are you sure you want to exit?",
+#             QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+#
+#         if odp == QMessageBox.Yes:
+#             event.accept()
+#         else:
+#             event.ignore()
+#
+#     def keyPressEvent(self, e):
+#         if e.key() == Qt.Key_Escape:
+#             self.close()
 
 
 if __name__ == '__main__':
